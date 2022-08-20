23b8cad841a43cd769a2243cdc810253f1528a98
aymks <76121581+aymks@users.noreply.github.com>
2022-08-20 17:37:49 +0000

    Deploying to gh-pages from @ aymks/restoApp@d53a040c9cb9bab1863b193e7e8899aa82cc3f5d 🚀

--- _next/static/rbmv653li7J7Bt67n0gsM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/577a88fd5236ca2a.css","static/chunks/pages/index-3bdd9b27570db637.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
---
+++ _next/static/xDalHelVL6-PF0BWPiORL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/99e7bf73c07aaa67.css","static/chunks/pages/index-c049ba92860d8f4c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
