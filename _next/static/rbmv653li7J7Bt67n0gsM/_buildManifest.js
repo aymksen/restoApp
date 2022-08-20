5b78fb905b2d3d09f8dca4fa02af78cb99b6b5ca
aymks <76121581+aymks@users.noreply.github.com>
2022-08-20 16:35:53 +0000

    Deploying to gh-pages from @ aymks/restoApp@e77452af334d5e2debddc6c5b5d79399c6e363d1 🚀

---
+++ _next/static/rbmv653li7J7Bt67n0gsM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/577a88fd5236ca2a.css","static/chunks/pages/index-3bdd9b27570db637.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
