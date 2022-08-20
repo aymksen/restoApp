2574e948db413dbaa2b595a51365b576a97f81de
Aymen <76121581+aymks@users.noreply.github.com>
2022-08-20 01:21:39 +0100

    2.1

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from "../styles/PizzaCard.module.css"
 import Image from 'next/dist/client/image'
+const prefix = '/restoApp'
+
 function PizzaCard() {
   return (
     <div className={styles.container}>
-      <Image src="/img/log2.png" alt="" width="500" height="500"/>
+      <Image src={prefix + "/img/log2.png"} alt="" width="500" height="500"/>
       
     </div>
   )
