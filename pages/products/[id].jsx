bb10edf60e91e6f76b25123ee2105e8ff9c23785
Aymen <76121581+aymks@users.noreply.github.com>
2022-08-22 15:17:28 +0100

    1.03

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import styles from "../styles/product.module.css"
+import React from "react";
+import styles from "../styles/product.module.css";
+
+const pizza = {
+  id: 1,
+  img: "/img/mozza.png",
+  name: "Viande hachee",
+  price: [35.0, 40.0, 45.0],
+  desc: "Best mexico desc",
+};
+
 function product() {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.left}>
+        <div className={styles.imgcontainer}>
+        <Image  src={pizza.img} alt="" width="500" height="500" objectFit="contain"/>
+        </div>
+      </div>
+      <div className={styles.right}></div>
     </div>
-  )
+  );
 }
 
-export default product
+export default product;
